2eaa551d38890b13902e5fd3b55d2fbe43b36c15
nuve-ra <nuve19ec@cmrit.ac.in>
2024-10-28 23:42:46 +0530

    fixed errors

--- src/common/aws.jsx
+++ src/common/aws.jsx
@@ -3,7 +3,7 @@ import axios from "axios"
 const uploadImage = async(img) =>{
 
     let imgURL= null;
-    await axios.get("http://localhost:3000/get-upload-url")
+    await axios.get(`${import.meta.env.VITE_SERVER_DOMAIN}/get-upload-url`)
     .then(async({ data: {uploadURL}}) =>{
         await axios({
             method : 'PUT',
